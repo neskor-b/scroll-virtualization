565583333cc4fcd0e419a5de63220964d79636e2
Bohdan <bogdaneskor@gmail.com>
2024-02-24 21:49:23 +0200

    add sentinel

--- src/components/VirtualScroll/index.tsx
+++ src/components/VirtualScroll/index.tsx
@@ -13,36 +13,55 @@ type Props = {
     onScrollDown: () => void;
 }
 
+const TOP_SENTINEL_ID = 'top-sentinel';
+const BOTTOM_SENTINEL_ID = 'bottom-sentinel';
 
 const VirtualScroll: FC<Props> = ({ data, uniqueKey, itemFixedHeight, unobserve, itemRender, onScrollDown }) => {
-    const lastElementRef = useRef(null);
+    const bottomSentinelRef = useRef(null);
+    const topSentinelRef = useRef(null);
 
     const observer = new IntersectionObserver((entries) => {
         const [entry] = entries;
         if (entry.isIntersecting) {
-            onScrollDown();
+            if (entry.target.id === BOTTOM_SENTINEL_ID) {
+                onScrollDown();
+            }
+            if (entry.target.id === TOP_SENTINEL_ID) {
+                console.log('top');
+            }
         }
     }, { threshold: 1.0, rootMargin: `${itemFixedHeight * 3}px` });
     
+    
 
     useEffect(() => {
-        if (lastElementRef.current) {
+        if (topSentinelRef.current) {
             if (unobserve) {
-                observer.unobserve(lastElementRef.current)
+                observer.unobserve(topSentinelRef.current)
             } else {
-                observer.observe(lastElementRef.current)
+                observer.observe(topSentinelRef.current)
             }
         }
+        if (bottomSentinelRef.current) {
+            if (unobserve) {
+                observer.unobserve(bottomSentinelRef.current)
+            } else {
+                observer.observe(bottomSentinelRef.current)
+            }
+        }
+
         return () => {
-            lastElementRef.current && observer.unobserve(lastElementRef.current);
+            bottomSentinelRef.current && observer.unobserve(bottomSentinelRef.current);
+            topSentinelRef.current && observer.unobserve(topSentinelRef.current);
         }
     }, [data, unobserve])
 
     return (
         <>
             <Stack spacing="10px">
+                {data.length > 0 && <Box id={TOP_SENTINEL_ID} ref={topSentinelRef} />}
                 {data.map((item, index) => <Box height={`${itemFixedHeight}px`} key={`${item[uniqueKey]}-${index}`}>{itemRender(item, index)}</Box>)}
-                {data.length > 0 && <Box ref={lastElementRef} />}
+                {data.length > 0 && <Box id={BOTTOM_SENTINEL_ID} ref={bottomSentinelRef} />}
             </Stack>
         </>
     )
--- src/hooks/useArticle.ts
+++ src/hooks/useArticle.ts
@@ -23,24 +23,43 @@ const useArticles = () => {
     const [hasNext, setHasNextPage] = useState(true);
     const [params, setParams] = useState<Params>({ page: 1, pageSize: 10, q: '' });
 
-    const fetchArticles = async (withrReset: boolean = true) => {
-        setIsLoading(true);
-        try {
-            withrReset && setArticles([]);
-            const { data } = await apiGetNews(params);
-            if (withrReset) {
-                setArticles(data.articles);
-            } else {
-                setArticles(prevArticles => [...prevArticles, ...data.articles]);
-            }
-            data.articles.length === 0 ? setHasNextPage(false) : setHasNextPage(true);
-        } catch (error) {
-            setIsError(true);
-            setHasNextPage(false);
-            console.error(error);
-        } finally {
-            setIsLoading(false);
-        }
+    // const fetchArticles = async (withrReset: boolean = true) => {
+    //     setIsLoading(true);
+    //     try {
+    //         withrReset && setArticles([]);
+    //         const { data } = await apiGetNews(params);
+    //         if (withrReset) {
+    //             setArticles(data.articles);
+    //         } else {
+    //             setArticles(prevArticles => [...prevArticles, ...data.articles]);
+    //         }
+    //         data.articles.length === 0 ? setHasNextPage(false) : setHasNextPage(true);
+    //     } catch (error) {
+    //         setIsError(true);
+    //         setHasNextPage(false);
+    //         console.error(error);
+    //     } finally {
+    //         setIsLoading(false);
+    //     }
+    // }
+
+    // TODO MOCK - delete
+    const fetchArticles = (test: boolean = true) => {
+        const newRandomArticles = Array.from({ length: 10 }, () => ({
+            source: {
+                id: '1',
+                name: '1',
+            },
+            author: '1',
+            title: '1',
+            description: '1',
+            url: '1',
+            urlToImage: '1',
+            publishedAt: '1',
+            content: '1'
+        }))
+        
+        setArticles(prevArticles => [...prevArticles, ...newRandomArticles]);
     }
 
     const changeParam = (paramName: keyof Params) => (value: string | number) => {
@@ -56,7 +75,7 @@ const useArticles = () => {
     }, [params.q])
 
     useUpdateEffect(() => {
-        fetchArticles(false);
+        fetchArticles(false);        
     }, [params.page])
 
     return { articles, isError, isLoading, params, hasNext, fetchArticles, changeParam };
